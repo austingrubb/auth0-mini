a2be25a317e03042c72de7c732a61a4b761ffd50
austin <austin.grubb.1995@gmail.com>
2018-09-05 12:14:46 -0700

    done with step 2

--- src/App.js
+++ src/App.js
@@ -14,8 +14,18 @@ class App extends Component {
     this.fetchSecureData = this.fetchSecureData.bind(this);
   }
 
+  componentDidMount(){
+    axios.get('/api/user-data').then(response => {
+      const user = response.data.user
+      this.setState({user})
+    })
+  }
+  
+
   login() {
-    alert('Need to implement the login() method in App.js!');
+    const redirectUri = encodeURIComponent(window.location.origin + '/auth/callback')
+    const auth0Url = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile$20email&redirect_uri=${redirectUri}&response_type=code`
+    window.location = auth0Url
   }
 
   logout() {
